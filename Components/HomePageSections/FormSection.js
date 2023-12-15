2214171e3215e965325fde5989c48c23cf0bc78a
developercodingarrow <digitalcloudinfo@gmail.com>
2023-12-15 15:51:46 +0530

    home page form connect to api

--- Components/HomePageSections/FormSection.js
+++ Components/HomePageSections/FormSection.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./css/formsection.module.css";
+import { useForm, Controller } from "react-hook-form";
+import { EnqureContext } from "../../contextApi/EnquireyContextApi";
+import toast, { Toaster } from "react-hot-toast";
+
 export default function FormSection() {
+  const { handelNewEnquire, isLoading, setisLoading } =
+    useContext(EnqureContext);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    control,
+    watch,
+  } = useForm({
+    mode: "all", // Use "onChange" mode for real-time validation as the user types
+  });
+
+  const handleFormSubmit = async (formdata) => {
+    try {
+      setisLoading(true);
+      const result = await handelNewEnquire(formdata);
+      console.log(result);
+      if (result.data.status === "Success") {
+        toast.success(result.data.message);
+        setisLoading(false);
+      } else if (result.data.status === "Error") {
+        toast.error(result.data.message);
+        setisLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className={styles.FormSection_mainContainer}>
@@ -64,23 +98,60 @@ export default function FormSection() {
                   Houzez CRM
                 </p>
               </div>
-              <form className={styles.form_part}>
+              <form
+                className={styles.form_part}
+                onSubmit={handleSubmit(handleFormSubmit)}
+              >
                 <div className={styles.input_box}>
                   <input
                     placeholder="First Name"
                     className={styles.input_style}
+                    name="name"
+                    {...register("name", {
+                      required: true,
+                    })}
                   />
                 </div>
+
+                {errors.name && (
+                  <p className={styles.errorMsg}>{"Name Is required"}</p>
+                )}
+
                 <div className={styles.input_box}>
                   <input
-                    placeholder="Email Address"
+                    placeholder="Number"
                     className={styles.input_style}
+                    name="number"
+                    {...register("number", {
+                      required: true,
+                    })}
                   />
                 </div>
+
+                {errors.number && (
+                  <p className={styles.errorMsg}>{"Number Is required"}</p>
+                )}
+
+                <div className={styles.input_box}>
+                  <input
+                    placeholder="Email"
+                    className={styles.input_style}
+                    name="email"
+                    {...register("email", {
+                      required: true,
+                    })}
+                  />
+                </div>
+
+                {errors.email && (
+                  <p className={styles.errorMsg}>{"Email Is required"}</p>
+                )}
                 <div className={styles.input_box}>
                   <textarea
-                    placeholder="Email Address"
+                    placeholder="Message"
                     className={styles.textarea_style}
+                    name="message"
+                    {...register("message")}
                   />
                 </div>
 
